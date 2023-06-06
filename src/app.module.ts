955f55a17b93453a1e54671f7360c132416c7111
Nitesh Ramola <nitesh.ramola@shippigo.com>
2023-06-06 12:28:27 +0530

    dependency injection b/w different modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import { ComputerModule } from './computer/computer.module';
+import { DiskModule } from './disk/disk.module';
+import { PowerModule } from './power/power.module';
+import { CpuModule } from './cpu/cpu.module';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [MessagesModule, ComputerModule, DiskModule, PowerModule, CpuModule],
   controllers: [AppController],
   providers: [AppService],
 })
